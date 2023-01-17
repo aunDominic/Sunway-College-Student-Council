99d361a674a15c45d5ae741f62decacba9c8d18b
Dominic <dominator1356@gmail.com>
2023-01-17 23:22:00 +0800

    '2nd'

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 let accordion = document.getElementsByClassName("accordion");
 
-for (i = 0; i < accordion.length; i++){
+
+for (let i = 0; i < accordion.length; i++){
     console.log(accordion[i])
-    accordion[i].addEventListener("click", function (){
+    accordion[i].addEventListener("click", function(){
         this.classList.toggle('open');
-    })
+    });
 }
+
+let years = document.getElementsByClassName("years");
+for (let i = 0; i < years.length; i++){
+    years[i].addEventListener('click',function(){
+        this.parentElement.classList.toggle('open');
+    });
+}
+let toggleSidebar = document.getElementsByClassName("toggle-sidebar");
+let sidebar = document.getElementsByClassName('sidebar');
+let content = document.getElementsByClassName("content");
+let smallSidebar = document.getElementsByClassName("small-sidebar");
+toggleSidebar[0].addEventListener('click', function(){
+    sidebar[0].classList.toggle('close-sidebar');
+    content[0].classList.toggle("open-sidebar");
+    smallSidebar[0].classList.toggle('close-sidebar');
+})
+smallSidebar[0].addEventListener('click', function(){
+    sidebar[0].classList.toggle('close-sidebar');
+    content[0].classList.toggle("open-sidebar");
+    smallSidebar[0].classList.toggle('close-sidebar');
+})
+
+
 
