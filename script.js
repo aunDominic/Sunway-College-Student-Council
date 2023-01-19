4988b00a65fd869b6ed85163c79905c3694473ab
Dominic <dominator1356@gmail.com>
2023-01-19 14:16:45 +0800

    'as'

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let accordion = document.getElementsByClassName("accordion");
 
+function toggle_sidebar(){
+    sidebar[0].classList.toggle('close-sidebar');
+    content[0].classList.toggle("open-sidebar");
+    smallSidebar[0].parentElement.classList.toggle('close-sidebar');
+}
+
+
 
 for (let i = 0; i < accordion.length; i++){
     console.log(accordion[i])
@@ -17,17 +24,39 @@ for (let i = 0; i < years.length; i++){
 let toggleSidebar = document.getElementsByClassName("toggle-sidebar");
 let sidebar = document.getElementsByClassName('sidebar');
 let content = document.getElementsByClassName("content");
-let smallSidebar = document.getElementsByClassName("small-sidebar");
-toggleSidebar[0].addEventListener('click', function(){
-    sidebar[0].classList.toggle('close-sidebar');
-    content[0].classList.toggle("open-sidebar");
-    smallSidebar[0].classList.toggle('close-sidebar');
+let smallSidebar = document.getElementsByClassName("toggle-small-sidebar");
+toggleSidebar[0].addEventListener('click', toggle_sidebar)
+smallSidebar[0].addEventListener('click', toggle_sidebar)
+content[0].addEventListener('click', function(){
+    if (content[0].classList.contains('open-sidebar')){
+        toggle_sidebar();
+    }
 })
-smallSidebar[0].addEventListener('click', function(){
-    sidebar[0].classList.toggle('close-sidebar');
-    content[0].classList.toggle("open-sidebar");
-    smallSidebar[0].classList.toggle('close-sidebar');
-})
+
+
+
+/* Select which SCC Term user wants to see*/
+let terms = document.getElementsByClassName("terms");
+
+for (let i = 0; i < terms.length; i++){
+    let term = terms[i].children;
+    for (let j = 0; j < term.length; j++)
+    {
+        term[j].addEventListener('click', function(){
+            let activeTerm = document.getElementsByClassName('active-term');
+            activeTerm[0].classList.toggle('active-term');
+            term[j].classList.toggle('active-term');
+
+            
+            let displayedContent = document.getElementsByClassName('display-content');
+            if (displayedContent.length > 0) displayedContent[0].classList.toggle('display-content');
+            let contentTerm = document.getElementsByClassName(term[j].id);
+            contentTerm[0].classList.toggle('display-content');
+
+            toggle_sidebar();
+        })
+    }
+}
 
 
 
